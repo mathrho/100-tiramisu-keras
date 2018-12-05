b2de12505ad69854bd5d33efbf240b5568250143
Zhenyang <zhenyang@example.com>
2018-12-05 21:16:53 +0800

    update jimat code

--- train_jimat.py
+++ train_jimat.py
@@ -98,7 +98,6 @@ def main(args=None):
     x = create_tiramisu(args.nb_classes, img_input)
     model = Model(img_input, x)
     print(model.summary())
-    return
 
     if not args.train_from_zero:
         model.load_weights(args.path_to_model_weights)

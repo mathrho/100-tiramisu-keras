ba5b5d6eaf98d63ccd24ae02d12a686bac677a8f
Zhenyang <zhenyang@example.com>
2018-12-05 20:55:53 +0800

    update jimat code

--- tiramisu/model.py
+++ tiramisu/model.py
@@ -73,8 +73,11 @@ def create_tiramisu(nb_classes, img_input, nb_dense_block=6,
 
     x = conv(img_input, nb_filter, 3, wd, 0)
     skips, added = down_path(x, nb_layers, growth_rate, p, wd)
-    print(skips.output_shape)
-    print(added.output_shape)
+    for l in skips:
+        print(l.output_shape)
+    print('-----------------')
+    for l in added:
+        print(l.output_shape)
     x = up_path(added, reverse(skips[:-1]), reverse(nb_layers[:-1]), growth_rate, p, wd)
 
     x = conv(x, nb_classes, 1, wd, 0)
